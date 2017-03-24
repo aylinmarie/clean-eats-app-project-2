55a2c2fe61581d907fd261195cd3b3bf6426a5aa
aylinmarie <aylinmariedesigns@gmail.com>
2017-03-24 15:31:38 -0400

    create starter code

---
+++ app.js
@@ -0,0 +1,69 @@
+pry = require('pryjs');
+var express = require('express');
+var mongoose = require('mongoose');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var methodOverride = require('method-override');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+
+mongoose.connect('mongodb://localhost/auth');
+var db = mongoose.connection;
+
+// Controllers
+var usersController = require('./routes/users.js');
+var sessionsController = require('./routes/sessions.js');
+var listsController = require("./routes/lists.js");
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+app.use(session({
+  secret: "derpderpderpcats",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.use('/users', usersController);
+app.use('/sessions', sessionsController);
+app.use("/users/:userId/lists", listsController);
+
+module.exports = app;
---
+++ db/schema.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.Promise = global.Promise;
+
+var ListSchema = new Schema({
+  name: String,
+  completed: Boolean
+})
+
+var UserSchema = new Schema({
+  username: String,
+  email: String,
+  password_digest: String,
+  list: [ListSchema],
+  created_at: Date,
+  updated_at: Date
+});
+
+
+//add a ListSchema.pre function (just like UserSchema below)
+//your code
+
+UserSchema.pre('save', function(next) {
+  now = new Date();
+  this.updated_at = now;
+
+  if (!this.created_at) { this.created_at = now }
+  next();
+});
+
+var UserModel = mongoose.model('User', UserSchema);
+var ListModel = mongoose.model("List", ListSchema);
+
+
+//your code
+
+//export List below
+module.exports = {
+  User: UserModel,
+  List: ListModel
+};
