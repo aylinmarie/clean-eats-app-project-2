5658c75e2454a82639463f73756d61d8de8ae9ca
aylinmarie <aylinmariedesigns@gmail.com>
2017-03-26 22:23:56 -0400

    heroku db

--- seeds.js
+++ seeds.js
@@ -5,6 +5,24 @@ var User = require("./models/user");
 
 mongoose.promise = global.Promise;
 
+// Connect to DB
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/express-movies');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
+
+// DATA
+
 Restaurant.remove({}, function(err) {
     console.log(err);
 });
